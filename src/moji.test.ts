1f6e562eefe245e1c64da0a3930a3d45b90e9d5b
ytoune <ytoune@users.noreply.github.com>
2023-12-16 12:07:46 +0900

    chore

--- src/moji.test.ts
+++ src/moji.test.ts
@@ -7,146 +7,146 @@ import { kana } from './map'
 import { moji, zenkaku2hankaku as orig } from './moji'
 
 describe('map', () => {
-	it('ok', () => {
-		expect(kana.HE.end - kana.HE.start).toBe(kana.ZE.end - kana.ZE.start)
-		expect(kana.KK.end - kana.KK.start).toBe(kana.HG.end - kana.HG.start)
-	})
+  it('ok', () => {
+    expect(kana.HE.end - kana.HE.start).toBe(kana.ZE.end - kana.ZE.start)
+    expect(kana.KK.end - kana.KK.start).toBe(kana.HG.end - kana.HG.start)
+  })
 })
 
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 const moji2 = ('function' === typeof Moji ? Moji : Moji.default) as typeof moji
 const zenkaku2hankaku = (str: string) => {
-	const u = moji2(str)
-		.convert('ZE', 'HE')
-		.convert('ZS', 'HS')
-		.toString()
-		.replace(/[ー－]/giu, '-')
-		.replace(/[、]/giu, '､')
-	const v = orig(str)
-	const { stringify: e } = JSON
-	if (v !== u) throw new Error(`? ${e(v)} !== ${e(u)}`)
-	return v
+  const u = moji2(str)
+    .convert('ZE', 'HE')
+    .convert('ZS', 'HS')
+    .toString()
+    .replace(/[ー－]/giu, '-')
+    .replace(/[、]/giu, '､')
+  const v = orig(str)
+  const { stringify: e } = JSON
+  if (v !== u) throw new Error(`? ${e(v)} !== ${e(u)}`)
+  return v
 }
 
 describe('moji', () => {
-	it('{', () => {
-		const result = moji2('{').convert('HE', 'ZE').toString()
-		expect(result).toBe('｛')
-	})
-	it('{', () => {
-		const result = moji('{').convert('HE', 'ZE').toString()
-		expect(result).toBe('｛')
-	})
-	it('！！！ ZE to HE', () => {
-		const result = moji('！！！').convert('ZE', 'HE').toString()
-		expect(result).toBe('!!!')
-	})
-	it('！！！ ZSn to HSn', () => {
-		const result = moji('！！！').convert('ZSn', 'HSn').toString()
-		expect(result).toBe('!!!')
-	})
-	it('｛〘', () => {
-		const result = moji('｛〘').convert('ZSn', 'HSn').toString()
-		expect(result).toBe('{⟬')
-	})
-	it('ＨＯ、，ＧＥ', () => {
-		const result = moji('ＨＯ、，ＧＥ').convert('ZE', 'HE').convert('ZSn', 'HSn').toString()
-		expect(result).toBe('HO､,GE')
-	})
+  it('{', () => {
+    const result = moji2('{').convert('HE', 'ZE').toString()
+    expect(result).toBe('｛')
+  })
+  it('{', () => {
+    const result = moji('{').convert('HE', 'ZE').toString()
+    expect(result).toBe('｛')
+  })
+  it('！！！ ZE to HE', () => {
+    const result = moji('！！！').convert('ZE', 'HE').toString()
+    expect(result).toBe('!!!')
+  })
+  it('！！！ ZSn to HSn', () => {
+    const result = moji('！！！').convert('ZSn', 'HSn').toString()
+    expect(result).toBe('!!!')
+  })
+  it('｛〘', () => {
+    const result = moji('｛〘').convert('ZSn', 'HSn').toString()
+    expect(result).toBe('{⟬')
+  })
+  it('ＨＯ、，ＧＥ', () => {
+    const result = moji('ＨＯ、，ＧＥ').convert('ZE', 'HE').convert('ZSn', 'HSn').toString()
+    expect(result).toBe('HO､,GE')
+  })
 })
 
 describe('core', () => {
-	const assert = {
-		strictEqual: (q: string, w: string) => {
-			expect(q).toBe(w)
-		},
-	}
+  const assert = {
+    strictEqual: (q: string, w: string) => {
+      expect(q).toBe(w)
+    },
+  }
 
-	it('全角英数から半角英数', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('ZE', 'HE').toString(),
-			'ABCD　01234あいうアイウABCD 01234ｱｲｳ',
-		)
-	})
+  it('全角英数から半角英数', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('ZE', 'HE').toString(),
+      'ABCD　01234あいうアイウABCD 01234ｱｲｳ',
+    )
+  })
 
-	it('全角スペースを半角スペースに', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('ZS', 'HS').toString(),
-			'ＡＢＣＤ ０１２３４あいうアイウABCD 01234ｱｲｳ',
-		)
-	})
+  it('全角スペースを半角スペースに', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('ZS', 'HS').toString(),
+      'ＡＢＣＤ ０１２３４あいうアイウABCD 01234ｱｲｳ',
+    )
+  })
 
-	it('半角スペースを全角スペースに', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HS', 'ZS').toString(),
-			'ＡＢＣＤ　０１２３４あいうアイウABCD　01234ｱｲｳ',
-		)
-	})
+  it('半角スペースを全角スペースに', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HS', 'ZS').toString(),
+      'ＡＢＣＤ　０１２３４あいうアイウABCD　01234ｱｲｳ',
+    )
+  })
 
-	it('半角英数から全角英数', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HE', 'ZE').toString(),
-			'ＡＢＣＤ　０１２３４あいうアイウＡＢＣＤ ０１２３４ｱｲｳ',
-		)
-	})
+  it('半角英数から全角英数', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HE', 'ZE').toString(),
+      'ＡＢＣＤ　０１２３４あいうアイウＡＢＣＤ ０１２３４ｱｲｳ',
+    )
+  })
 
-	it('全角カナから半角カナ', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('ZK', 'HK').toString(),
-			'ＡＢＣＤ　０１２３４あいうｱｲｳABCD 01234ｱｲｳ',
-		)
-	})
+  it('全角カナから半角カナ', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('ZK', 'HK').toString(),
+      'ＡＢＣＤ　０１２３４あいうｱｲｳABCD 01234ｱｲｳ',
+    )
+  })
 
-	it('半角カナから全角カナ', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HK', 'ZK').toString(),
-			'ＡＢＣＤ　０１２３４あいうアイウABCD 01234アイウ',
-		)
-	})
+  it('半角カナから全角カナ', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HK', 'ZK').toString(),
+      'ＡＢＣＤ　０１２３４あいうアイウABCD 01234アイウ',
+    )
+  })
 
-	it('ひらがなからカタカナ', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HG', 'KK').toString(),
-			'ＡＢＣＤ　０１２３４アイウアイウABCD 01234ｱｲｳ',
-		)
-	})
+  it('ひらがなからカタカナ', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HG', 'KK').toString(),
+      'ＡＢＣＤ　０１２３４アイウアイウABCD 01234ｱｲｳ',
+    )
+  })
 
-	it('カタカナからひらがな', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('KK', 'HG').toString(),
-			'ＡＢＣＤ　０１２３４あいうあいうABCD 01234ｱｲｳ',
-		)
-	})
+  it('カタカナからひらがな', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('KK', 'HG').toString(),
+      'ＡＢＣＤ　０１２３４あいうあいうABCD 01234ｱｲｳ',
+    )
+  })
 
-	it('複数の文字種を置換', () => {
-		assert.strictEqual(
-			moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HK', 'ZK').convert('KK', 'HG').toString(),
-			'ＡＢＣＤ　０１２３４あいうあいうABCD 01234あいう',
-		)
-	})
+  it('複数の文字種を置換', () => {
+    assert.strictEqual(
+      moji('ＡＢＣＤ　０１２３４あいうアイウABCD 01234ｱｲｳ').convert('HK', 'ZK').convert('KK', 'HG').toString(),
+      'ＡＢＣＤ　０１２３４あいうあいうABCD 01234あいう',
+    )
+  })
 })
 describe('zenkaku2hankaku', () => {
-	it('Ｃ２－５，ＮＴ', () => {
-		expect(zenkaku2hankaku('Ｃ２－５，ＮＴ')).toBe('C2-5,NT')
-	})
-	it('　ほＨＯＥＧＥ', () => {
-		expect(zenkaku2hankaku('　ほＨＯＥＧＥ')).toBe(' ほHOEGE')
-	})
-	it('ﾎｹﾞﾎｹﾞ', () => {
-		expect(zenkaku2hankaku('ﾎｹﾞﾎｹﾞ')).toBe('ﾎｹﾞﾎｹﾞ')
-	})
-	it('💘❤💓💔💕💖', () => {
-		expect(zenkaku2hankaku('💘❤💓💔💕💖')).toBe('💘❤💓💔💕💖')
-		expect('💘❤💓💔💕💖'.length).toBe('💘❤💓💔💕💖'.length)
-	})
-	it('comma', () => {
-		expect(zenkaku2hankaku('ＨＯ、，ＧＥ')).toBe('HO､,GE')
-	})
-	it('-ー－', () => {
-		expect(zenkaku2hankaku('-ー－')).toBe('---')
-	})
-	it('＋－＊／', () => {
-		expect(zenkaku2hankaku('＋－＊／')).toBe('+-*/')
-	})
+  it('Ｃ２－５，ＮＴ', () => {
+    expect(zenkaku2hankaku('Ｃ２－５，ＮＴ')).toBe('C2-5,NT')
+  })
+  it('　ほＨＯＥＧＥ', () => {
+    expect(zenkaku2hankaku('　ほＨＯＥＧＥ')).toBe(' ほHOEGE')
+  })
+  it('ﾎｹﾞﾎｹﾞ', () => {
+    expect(zenkaku2hankaku('ﾎｹﾞﾎｹﾞ')).toBe('ﾎｹﾞﾎｹﾞ')
+  })
+  it('💘❤💓💔💕💖', () => {
+    expect(zenkaku2hankaku('💘❤💓💔💕💖')).toBe('💘❤💓💔💕💖')
+    expect('💘❤💓💔💕💖'.length).toBe('💘❤💓💔💕💖'.length)
+  })
+  it('comma', () => {
+    expect(zenkaku2hankaku('ＨＯ、，ＧＥ')).toBe('HO､,GE')
+  })
+  it('-ー－', () => {
+    expect(zenkaku2hankaku('-ー－')).toBe('---')
+  })
+  it('＋－＊／', () => {
+    expect(zenkaku2hankaku('＋－＊／')).toBe('+-*/')
+  })
 })
--- src/moji.ts
+++ src/moji.ts
@@ -15,61 +15,64 @@ import { kana, map } from './map'
 export type Mojisyu = 'ZE' | 'HE' | 'ZS' | 'HS' | 'HG' | 'KK' | 'ZK' | 'HK'
 
 export type ConvertType =
-	| readonly ['ZE', 'HE']
-	| readonly ['HE', 'ZE']
-	| readonly ['ZS', 'HS']
-	| readonly ['HS', 'ZS']
-	| readonly ['ZK', 'HK']
-	| readonly ['HK', 'ZK']
-	| readonly ['ZSn', 'HSn'] // 記号(シンボル)
-	| readonly ['HSn', 'ZSn']
-	| readonly ['ZHg', 'HHg'] // ハングル
-	| readonly ['HHg', 'ZHg']
-	| readonly ['HG', 'KK']
-	| readonly ['KK', 'HG']
+  | readonly ['ZE', 'HE']
+  | readonly ['HE', 'ZE']
+  | readonly ['ZS', 'HS']
+  | readonly ['HS', 'ZS']
+  | readonly ['ZK', 'HK']
+  | readonly ['HK', 'ZK']
+  | readonly ['ZSn', 'HSn'] // 記号(シンボル)
+  | readonly ['HSn', 'ZSn']
+  | readonly ['ZHg', 'HHg'] // ハングル
+  | readonly ['HHg', 'ZHg']
+  | readonly ['HG', 'KK']
+  | readonly ['KK', 'HG']
 
 interface Moji {
-	convert(...args: ConvertType): Moji
-	// filter(type: Mojisyu): Moji
-	// reject(type: Mojisyu): Moji
-	toString(): string
+  convert(...args: ConvertType): Moji
+  // filter(type: Mojisyu): Moji
+  // reject(type: Mojisyu): Moji
+  toString(): string
 }
 
+/**
+ * @internal
+ */
 export const unreachable = (_: never): never => {
-	throw new Error('unreachable.')
+  throw new Error('unreachable.')
 }
 
 const u16 = (num: number) => num.toString(16).padStart(4, '0')
 const toRegStr = (pairs: readonly (readonly [string, string])[], i: 0 | 1) =>
-	`[${pairs.map(m => '\\u' + u16(m[i].codePointAt(0) || 1)).join('')}]`
+  `[${pairs.map(m => '\\u' + u16(m[i].codePointAt(0) || 1)).join('')}]`
 const regMap = {
-	ZE: new RegExp(`[\\u${u16(kana.ZE.start)}-\\u${u16(kana.ZE.end)}]`, 'g'),
-	HE: new RegExp(`[\\u${u16(kana.HE.start)}-\\u${u16(kana.HE.end)}]`, 'g'),
-	HG: new RegExp(`[\\u${u16(kana.HG.start)}-\\u${u16(kana.HG.end)}]`, 'g'),
-	KK: new RegExp(`[\\u${u16(kana.KK.start)}-\\u${u16(kana.KK.end)}]`, 'g'),
-	hSymbol: new RegExp(toRegStr([...map['ascii.symbol'], ...map['jis0201.symbol'], ...map['unicode.symbol']], 1), 'g'),
-	zSymbol: new RegExp(toRegStr([...map['ascii.symbol'], ...map['jis0201.symbol'], ...map['unicode.symbol']], 0), 'g'),
-	hHangul: new RegExp(toRegStr(map['unicode.hangul'], 1), 'g'),
-	zHangul: new RegExp(toRegStr(map['unicode.hangul'], 0), 'g'),
+  ZE: new RegExp(`[\\u${u16(kana.ZE.start)}-\\u${u16(kana.ZE.end)}]`, 'g'),
+  HE: new RegExp(`[\\u${u16(kana.HE.start)}-\\u${u16(kana.HE.end)}]`, 'g'),
+  HG: new RegExp(`[\\u${u16(kana.HG.start)}-\\u${u16(kana.HG.end)}]`, 'g'),
+  KK: new RegExp(`[\\u${u16(kana.KK.start)}-\\u${u16(kana.KK.end)}]`, 'g'),
+  hSymbol: new RegExp(toRegStr([...map['ascii.symbol'], ...map['jis0201.symbol'], ...map['unicode.symbol']], 1), 'g'),
+  zSymbol: new RegExp(toRegStr([...map['ascii.symbol'], ...map['jis0201.symbol'], ...map['unicode.symbol']], 0), 'g'),
+  hHangul: new RegExp(toRegStr(map['unicode.hangul'], 1), 'g'),
+  zHangul: new RegExp(toRegStr(map['unicode.hangul'], 0), 'g'),
 }
 
 const repMap = {
-	ZE2HE: (m: string): string => String.fromCodePoint(kana.HE.start + (m.codePointAt(0) || 0) - kana.ZE.start),
-	HE2ZE: (m: string): string => String.fromCodePoint(kana.ZE.start + (m.codePointAt(0) || 0) - kana.HE.start),
-	HG2KK: (m: string): string => String.fromCodePoint(kana.KK.start + (m.codePointAt(0) || 0) - kana.HG.start),
-	KK2HG: (m: string): string => String.fromCodePoint(kana.HG.start + (m.codePointAt(0) || 0) - kana.KK.start),
+  ZE2HE: (m: string): string => String.fromCodePoint(kana.HE.start + (m.codePointAt(0) || 0) - kana.ZE.start),
+  HE2ZE: (m: string): string => String.fromCodePoint(kana.ZE.start + (m.codePointAt(0) || 0) - kana.HE.start),
+  HG2KK: (m: string): string => String.fromCodePoint(kana.KK.start + (m.codePointAt(0) || 0) - kana.HG.start),
+  KK2HG: (m: string): string => String.fromCodePoint(kana.HG.start + (m.codePointAt(0) || 0) - kana.KK.start),
 }
 
 const pushMap = (pairs: readonly (readonly [string, string])[], i: 0 | 1, j: 0 | 1, map: Map<string, string>) => {
-	for (const pair of pairs) map.set(pair[i], pair[j])
+  for (const pair of pairs) map.set(pair[i], pair[j])
 }
 const mapMap = {
-	ZK2HK: new Map<string, string>(),
-	HK2ZK: new Map<string, string>(),
-	ZSn2HSn: new Map<string, string>(),
-	HSn2ZSn: new Map<string, string>(),
-	ZHg2HHg: new Map<string, string>(),
-	HHg2ZHg: new Map<string, string>(),
+  ZK2HK: new Map<string, string>(),
+  HK2ZK: new Map<string, string>(),
+  ZSn2HSn: new Map<string, string>(),
+  HSn2ZSn: new Map<string, string>(),
+  ZHg2HHg: new Map<string, string>(),
+  HHg2ZHg: new Map<string, string>(),
 } as const
 for (const [i, c] of kana.ZK.list.entries()) mapMap.ZK2HK.set(c, kana.HK.list[i] ?? '?')
 for (const [i, c] of kana.HK.list.entries()) mapMap.HK2ZK.set(c, kana.ZK.list[i] ?? '?')
@@ -79,37 +82,37 @@ pushMap(map['unicode.hangul'], 0, 1, mapMap.ZHg2HHg)
 pushMap(map['unicode.hangul'], 1, 0, mapMap.HHg2ZHg)
 
 const mojiImpl = (moji: string): Moji => ({
-	toString: (): string => moji,
-	convert: (...args: ConvertType): Moji => {
-		switch (args[0]) {
-			case 'ZE':
-				return mojiImpl(moji.replace(regMap.ZE, repMap.ZE2HE))
-			case 'HE':
-				return mojiImpl(moji.replace(regMap.HE, repMap.HE2ZE))
-			case 'KK':
-				return mojiImpl(moji.replace(regMap.KK, repMap.KK2HG))
-			case 'HG':
-				return mojiImpl(moji.replace(regMap.HG, repMap.HG2KK))
-			case 'ZS':
-				return mojiImpl(moji.replace(kana.ZS.patterns[0][0], kana.ZS.patterns[0][1].HS))
-			case 'HS':
-				return mojiImpl(moji.replace(kana.HS.patterns[0][0], kana.HS.patterns[0][1].ZS))
-			case 'ZK':
-				return mojiImpl(moji.replace(kana.ZK.regexp, m => mapMap.ZK2HK.get(m) || '?'))
-			case 'HK':
-				return mojiImpl(moji.replace(kana.HK.regexp, m => mapMap.HK2ZK.get(m) || '?'))
-			case 'ZSn':
-				return mojiImpl(moji.replace(regMap.zSymbol, m => mapMap.ZSn2HSn.get(m) || '?'))
-			case 'HSn':
-				return mojiImpl(moji.replace(regMap.hSymbol, m => mapMap.HSn2ZSn.get(m) || '?'))
-			case 'ZHg':
-				return mojiImpl(moji.replace(regMap.zHangul, m => mapMap.ZHg2HHg.get(m) || '?'))
-			case 'HHg':
-				return mojiImpl(moji.replace(regMap.hHangul, m => mapMap.HHg2ZHg.get(m) || '?'))
-			default:
-				return unreachable(args)
-		}
-	},
+  toString: (): string => moji,
+  convert: (...args: ConvertType): Moji => {
+    switch (args[0]) {
+      case 'ZE':
+        return mojiImpl(moji.replace(regMap.ZE, repMap.ZE2HE))
+      case 'HE':
+        return mojiImpl(moji.replace(regMap.HE, repMap.HE2ZE))
+      case 'KK':
+        return mojiImpl(moji.replace(regMap.KK, repMap.KK2HG))
+      case 'HG':
+        return mojiImpl(moji.replace(regMap.HG, repMap.HG2KK))
+      case 'ZS':
+        return mojiImpl(moji.replace(kana.ZS.patterns[0][0], kana.ZS.patterns[0][1].HS))
+      case 'HS':
+        return mojiImpl(moji.replace(kana.HS.patterns[0][0], kana.HS.patterns[0][1].ZS))
+      case 'ZK':
+        return mojiImpl(moji.replace(kana.ZK.regexp, m => mapMap.ZK2HK.get(m) || '?'))
+      case 'HK':
+        return mojiImpl(moji.replace(kana.HK.regexp, m => mapMap.HK2ZK.get(m) || '?'))
+      case 'ZSn':
+        return mojiImpl(moji.replace(regMap.zSymbol, m => mapMap.ZSn2HSn.get(m) || '?'))
+      case 'HSn':
+        return mojiImpl(moji.replace(regMap.hSymbol, m => mapMap.HSn2ZSn.get(m) || '?'))
+      case 'ZHg':
+        return mojiImpl(moji.replace(regMap.zHangul, m => mapMap.ZHg2HHg.get(m) || '?'))
+      case 'HHg':
+        return mojiImpl(moji.replace(regMap.hHangul, m => mapMap.HHg2ZHg.get(m) || '?'))
+      default:
+        return unreachable(args)
+    }
+  },
 })
 
 export const moji = mojiImpl
@@ -118,12 +121,12 @@ export const moji = mojiImpl
  * 全角英数字を半角にする
  */
 export const zenkaku2hankaku = (str: string) =>
-	mojiImpl(str)
-		.convert('ZE', 'HE')
-		.convert('ZS', 'HS')
-		.convert('ZSn', 'HSn')
-		.convert('ZHg', 'HHg')
-		.toString()
-		.replace(/[ー－]/giu, '-')
+  mojiImpl(str)
+    .convert('ZE', 'HE')
+    .convert('ZS', 'HS')
+    .convert('ZSn', 'HSn')
+    .convert('ZHg', 'HHg')
+    .toString()
+    .replace(/[ー－]/giu, '-')
 
 export const toLowerHankaku = (str: string) => zenkaku2hankaku(str).toLowerCase()
